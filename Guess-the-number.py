9b53fdd8687150ac90f2de82ce84bdb3c4c53b6e
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2021-12-05 14:58:09 +0800

    While loop and if statement if the input is greater than the random number.

--- Guess-the-number.py
+++ Guess-the-number.py
@@ -13,3 +13,10 @@ from random import randrange
 randomNum = randrange(0, 100)
 
 userGuess = int(input("\nEnter your guess here: "))
+
+# a code block that will ask the user repeatedly until the random number has been guessed correctly
+while randomNum != userGuess:
+    if userGuess > randomNum:
+        userGuess = int(input("Your input number is greater than the number. Try decreasing your numerical input. \n\nEnter your guess here: "))
+        if userGuess == randomNum:
+            break
