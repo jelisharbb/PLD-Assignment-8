eb2b9e4678acc633ab32c0ed10dd10b050fe16fd
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2021-12-08 15:15:34 +0800

    Added text colors and spaces.

--- Lottery.py
+++ Lottery.py
@@ -29,69 +29,69 @@ while restart:
             luckyNumDup = True
             break
     # list of lucky numbers    
-    luckyNum123 = luckyNum1, luckyNum2, luckyNum3
+    luckyNumList = luckyNum1, luckyNum2, luckyNum3
 
     # greeting to the user
-    print ("\nWELCOME TO PEPITO MANA-LOTTERY \n")
+    print ("\n\033[1m| WELCOME TO PEPITO MANA-LOTTERY |\033[0m\n")
 
     # looping for 1st number (if the user input is either an alphabet or a number beyond the scope)
     userNum1Loop = False
     while not(userNum1Loop):
-        userNum1 = input("(1) Pick a number from 0 to 9  : ")
+        userNum1 = input("\033[1m\033[96m(1) Pick a number from 0 to 9  :\033[0m ")
         if userNum1.isdigit():
             userNum1Fnl = int(userNum1)
             if userNum1Fnl <= 9 and userNum1Fnl >= 0:
                 userNum1Loop = True
             else:
-                print ("Your input is beyond the scope of the lottery. Please choose a number from 0 to 9 only.")
+                print ("Your input is \033[4mbeyond the scope\033[0m of the lottery. Please choose a number from \033[4m0 to 9\033[0m only.")
                 userNum1Loop = False
         else:
-            print ("Please input numerical value only.")
+            print ("Please input \033[4mnumerical value\033[0m only.")
             userNum1Loop = False
 
     # looping for 2nd number (if the user input is either an alphabet, a number beyond the scope, or a duplicate number)
     userNum2Loop = False
     while not(userNum2Loop):
-        userNum2 = input("(2) Pick a number from 0 to 9  : ")
+        userNum2 = input("\033[1m\033[96m(2) Pick a number from 0 to 9  :\033[0m ")
         if userNum2.isdigit():
             userNum2Fnl = int(userNum2)
             if userNum2Fnl == userNum1Fnl:
-                print ("You cannot enter same numbers. Please choose another number.")
+                print ("You \033[4mcannot\033[0m enter \033[4msame\033[0m numbers. Please choose another number.")
                 userNum2Loop = False
             elif userNum2Fnl <= 9 and userNum2Fnl >= 0:
                 userNum2Loop = True
             else:
-                print ("Your input is beyond the scope of the lottery. Please choose a number from 0 to 9 only.")
+                print ("Your input is \033[4mbeyond the scope\033[0m of the lottery. Please choose a number from \033[4m0 to 9\033[0m only.")
                 userNum2Loop = False
         else:
-            print ("Please input numerical value only.")
+            print ("Please input \033[4mnumerical value\033[0m only.")
             userNum2Loop = False
 
     # looping for 3rd number (if the user input is either an alphabet, a number beyond the scope, or a duplicate number)
     userNum3Loop = False
     while not(userNum3Loop):
-        userNum3 = input("(3) Pick a number from 0 to 9  : ")
+        userNum3 = input("\033[1m\033[96m(3) Pick a number from 0 to 9  :\033[0m ")
         if userNum3.isdigit():
             userNum3Fnl = int(userNum3)
             if userNum3Fnl == userNum1Fnl or userNum3Fnl == userNum2Fnl:
-                print ("You cannot enter same numbers. Please choose another number.")
+                print ("You \033[4mcannot\033[0m enter \033[4msame\033[0m numbers. Please choose another number.")
                 userNum3Loop = False
             elif userNum3Fnl <= 9 and userNum3Fnl >= 0:
                 userNum3Loop = True
             else:
-                print ("Your input is beyond the scope of the lottery. Please choose a number from 0 to 9 only.")
+                print ("Your input is \033[4mbeyond the scope\033[0m of the lottery. Please choose a number from \033[4m0 to 9\033[0m only.")
                 userNum3Loop = False
         else:
-            print ("Please input numerical value only.")
+            print ("Please input \033[4mnumerical value\033[0m only.")
             userNum3Loop = False
 
     # list of user inputed numbers
-    userNum123 = int(userNum1), int(userNum2), int(userNum3)
-    print (f"Your numbers: {int(userNum1)}, {int(userNum2)}, {int(userNum3)}")
+    userNumList = int(userNum1), int(userNum2), int(userNum3)
+    print (f"\nYour numbers: {int(userNum1)}, {int(userNum2)}, {int(userNum3)}")
 
     # if the user won
-    if sorted(luckyNum123) == sorted(userNum123):
-        playAgain = input(f"You won! \nThe winning numbers are [{luckyNum1}, {luckyNum2}, {luckyNum3}] \nDo you want to play again? (Type Y if Yes and N if No) ")
+    if sorted(luckyNumList) == sorted(userNumList):
+        playAgain = input(f"\n\033[92m\033[1mY O U  W O N !\033[21m\033[0m \nThe winning numbers: {luckyNum1}, {luckyNum2}, {luckyNum3} \n\nDo you want to play again? (Type Y if Yes and N if No)\n")
         # if the user wants to play again
         if playAgain.upper() == 'Y':
             restart = True
@@ -101,11 +101,11 @@ while restart:
 
     # if the user lose
     else:
-        playAgain = input(f"You lose. \nThe winning numbers are [{luckyNum1}, {luckyNum2}, {luckyNum3}] \nDo you want to play again? (Type Y if Yes and N if No) ")
+        playAgain = input(f"\n\033[1m\033[91mY O U  L O S E .\033[0m \nThe winning numbers: {luckyNum1}, {luckyNum2}, {luckyNum3} \n\nDo you want to play again? (Type Y if Yes and N if No)\n")
         if playAgain.upper() == 'Y':
             restart = True
         elif playAgain.upper() == 'N':
             restart = False
             break
 
-print ("\nThank you for playing. Come again.\n")
+print ("\n\033[1m\033[95mTHANK YOU FOR PLAYING! COME AGAIN. (•ᴗ•)\033[0m\n")
